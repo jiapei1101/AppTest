ab5b8edbc72540ca712605f12f7f5d1294af635a
Craetive <jiapei1101@gmail.com>
2024-11-18 09:20:01 +0800

    Update MainActivity_w.kt

--- MainActivity_w.kt
+++ MainActivity_w.kt
@@ -1,83 +1,236 @@
-package com.example.monthlyyearlyfinance
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:background="#E9E0DD"
+    tools:context=".MainActivity_w">
 
-import android.app.Dialog
-import android.content.Intent
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.NumberPicker
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.*
+    <androidx.viewpager2.widget.ViewPager2
+        android:id="@+id/viewPager"
+        android:layout_width="348dp"
+        android:layout_height="150dp"
+        android:layout_marginEnd="35dp"
+        android:layout_marginBottom="19dp"
+        android:background="@drawable/rounded_corner"
+        android:shape="rectangle"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintEnd_toEndOf="parent" >
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    </androidx.viewpager2.widget.ViewPager2>
 
-        val choose_btn = findViewById<Button>(R.id.choose_btn)
-        choose_btn.setOnClickListener {
-            showYearMonthPickerDialog(choose_btn)  // 將按鈕傳遞到 dialog
-        }
+    <androidx.viewpager2.widget.ViewPager2
+        android:id="@+id/viewPager1"
+        android:layout_width="348dp"
+        android:layout_height="206dp"
+        android:layout_marginEnd="35dp"
+        android:layout_marginBottom="28dp"
+        android:background="@drawable/rounded_corner"
+        android:shape="rectangle"
+        app:layout_constraintBottom_toTopOf="@+id/outcome_pc"
+        app:layout_constraintEnd_toEndOf="parent" />
 
-        val monthBtn = findViewById<Button>(R.id.month_btn)
-        monthBtn.setOnClickListener {
-            // 創建 Intent 用來跳轉到 MainActivity
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)  // 啟動新頁面
-        }
-        val yearBtn = findViewById<Button>(R.id.year_btn)
-        yearBtn.setOnClickListener {
-            // 創建 Intent 用來跳轉到 MainActivity2
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)  // 啟動新頁面
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    <androidx.viewpager2.widget.ViewPager2
+        android:id="@+id/viewPager2"
+        android:layout_width="348dp"
+        android:layout_height="204dp"
+        android:layout_marginStart="28dp"
+        android:layout_marginBottom="88dp"
+        android:background="@drawable/rounded_corner"
+        android:shape="rectangle"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintStart_toStartOf="parent" />
 
-    private fun showYearMonthPickerDialog(choose_btn: Button) {
-        val dialog = Dialog(this)
-        val inflater = LayoutInflater.from(this)
-        val view = inflater.inflate(R.layout.dialog_year_month_picker, null)
-        dialog.setContentView(view)
 
-        val yearPicker = view.findViewById<NumberPicker>(R.id.year_picker)
-        val monthPicker = view.findViewById<NumberPicker>(R.id.month_picker)
-        val confirmButton = view.findViewById<Button>(R.id.confirm_button)
+    <TextView
+        android:id="@+id/income_pc"
+        android:layout_width="wrap_content"
+        android:layout_height="24dp"
+        android:layout_marginStart="32dp"
+        android:layout_marginBottom="16dp"
+        android:text="收入月趨勢圖"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@+id/viewPager1"
+        app:layout_constraintStart_toStartOf="parent" />
 
-        // 設置年份範圍，例如從 1900 年到當前年份
-        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-        yearPicker.minValue = 1900
-        yearPicker.maxValue = currentYear
-        yearPicker.value = currentYear
+    <TextView
+        android:id="@+id/outcome_pc"
+        android:layout_width="wrap_content"
+        android:layout_height="24dp"
+        android:layout_marginStart="32dp"
+        android:layout_marginBottom="16dp"
+        android:text="支出月趨勢圖"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@id/viewPager2"
+        app:layout_constraintStart_toStartOf="parent" />
 
-        // 設置月份範圍 1-12
-        monthPicker.minValue = 1
-        monthPicker.maxValue = 12
-        monthPicker.value = Calendar.getInstance().get(Calendar.MONTH) + 1
+    <TextView
+        android:id="@+id/outcome_wd"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="47dp"
+        android:layout_marginBottom="130dp"
+        android:text="共支出0筆，合計"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
 
-        // 確認按鈕點擊事件
-        confirmButton.setOnClickListener {
-            val selectedYear = yearPicker.value
-            val selectedMonth = monthPicker.value
-            showToast("你選擇的是：${selectedYear}年 ${selectedMonth}月")
+    <TextView
+        android:id="@+id/outcome_nt"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="47dp"
+        android:layout_marginBottom="94dp"
+        android:text="NT$"
+        android:textColor="#D35E5E"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
 
-            // 更新按鈕的文字為選擇的年和月
-            choose_btn.text = "${selectedYear}年 ${selectedMonth}月"
+    <TextView
+        android:id="@+id/outcome_num"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="81dp"
+        android:layout_marginBottom="92dp"
+        android:text="0"
+        android:textColor="#D35E5E"
+        android:textSize="24sp"
+        android:textStyle="bold"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
 
-            dialog.dismiss()
-        }
+    <TextView
+        android:id="@+id/income_wd"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="47dp"
+        android:layout_marginBottom="57dp"
+        android:text="0筆收入"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
 
-        dialog.show()
-    }
+    <TextView
+        android:id="@+id/income_num"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="145dp"
+        android:layout_marginBottom="55dp"
+        android:text="0"
+        android:textColor="#2A6FD7"
+        android:textSize="20sp"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
 
-    // 使用預設Toast顯示文字訊息
-    private fun showToast(text: String) =
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-}
+    <TextView
+        android:id="@+id/income_nt"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="110dp"
+        android:layout_marginBottom="57dp"
+        android:text="NT$"
+        android:textColor="#2A6FD7"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
+
+    <TextView
+        android:id="@+id/balance_wd"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="47dp"
+        android:layout_marginBottom="25dp"
+        android:text="結餘"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
+
+    <TextView
+        android:id="@+id/balance_nt"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="85dp"
+        android:layout_marginBottom="25dp"
+        android:text="NT$"
+        android:textSize="16sp"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
+
+    <TextView
+        android:id="@+id/balance_num"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="120dp"
+        android:layout_marginBottom="23dp"
+        android:text="0"
+        android:textSize="20sp"
+        app:layout_constraintBottom_toTopOf="@+id/income_pc"
+        app:layout_constraintStart_toStartOf="parent" />
+
+    <LinearLayout
+        android:layout_width="270dp"
+        android:layout_height="50dp"
+        android:layout_marginTop="21dp"
+        android:layout_marginEnd="64dp"
+        android:orientation="horizontal"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintTop_toTopOf="parent">
+
+        <Button
+            android:id="@+id/weal_btn"
+            android:layout_width="92dp"
+            android:layout_height="48dp"
+            android:text="週帳單" />
+
+        <Button
+            android:id="@+id/month_btn"
+            android:layout_width="89dp"
+            android:layout_height="49dp"
+            android:backgroundTint="#FFFFFF"
+            android:text="月帳單"
+            android:textStyle="bold" />
+
+        <Button
+            android:id="@+id/year_btn"
+            android:layout_width="88dp"
+            android:layout_height="49dp"
+            android:text="年帳單" />
+    </LinearLayout>
+
+    <Button
+        android:id="@+id/choose_btn_w"
+        android:layout_width="158dp"
+        android:layout_height="53dp"
+        android:layout_marginStart="60dp"
+        android:layout_marginTop="76dp"
+        android:background="#00000000"
+        android:text="2024年11月"
+        android:textColor="#2B2D30"
+        android:textSize="20sp"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
+
+    <com.github.mikephil.charting.charts.BarChart
+        android:id="@+id/barChart"
+        android:layout_width="346dp"
+        android:layout_height="152dp"
+        android:layout_marginEnd="36dp"
+        android:layout_marginBottom="385dp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        tools:ignore="MissingConstraints" />
+
+    <com.github.mikephil.charting.charts.BarChart
+        android:id="@+id/barChart2"
+        android:layout_width="347dp"
+        android:layout_height="154dp"
+        android:layout_marginEnd="36dp"
+        android:layout_marginBottom="115dp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        tools:ignore="MissingConstraints" />
+
+</androidx.constraintlayout.widget.ConstraintLayout>
